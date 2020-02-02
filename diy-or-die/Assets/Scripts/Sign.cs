4797dce341eba9f968b0c4bbcb30790498357ee3
minjeong <minjeongkim.digipen@gmail.com>
2020-02-01 18:49:14 -0800

    create Sign object passing by the car

---
+++ diy-or-die/Assets/Scripts/Sign.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+    public Animator animator;
+    public Text text;
+
+    private void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void PlayAnimation()
+    {
+        this.gameObject.SetActive(true);
+        animator.SetBool("Play", true);
+    }
+
+    public void ResetTrigger()
+    {
+        animator.SetBool("Play", false);
+        this.gameObject.SetActive(false);
+    }
+
+}
