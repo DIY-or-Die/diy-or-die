5a1f6dab892af1ca45a64c6e49e8413f28934511
minjeong <minjeongkim.digipen@gmail.com>
2020-02-02 09:44:31 -0800

    BackgroundImg Prefab

--- diy-or-die/Assets/Scripts/Sign.cs
+++ diy-or-die/Assets/Scripts/Sign.cs
@@ -8,25 +8,29 @@ public class Sign : MonoBehaviour
     public Animator animator;
     public Text text;
     public GameManager GM;
+    public bool AnimationAvailable;
 
     private void Start()
     {
         GM = FindObjectOfType<GameManager>();
         this.gameObject.SetActive(false);
+        AnimationAvailable = true;
     }
-
+                             
     public void PlayAnimation()
     {
         float remainingTime = GM.WinTimer - GM.WinTimeElapsed;
         text.text = "Goal\n"+ remainingTime.ToString("0.00") +" miles";
         this.gameObject.SetActive(true);
         animator.SetBool("Play", true);
+        AnimationAvailable = false;
     }
 
-    public void ResetTrigger()
+    public void ResetTrigger()                                                                                                                      
     {
         animator.SetBool("Play", false);
         this.gameObject.SetActive(false);
+        AnimationAvailable = true;
     }
 
 }
