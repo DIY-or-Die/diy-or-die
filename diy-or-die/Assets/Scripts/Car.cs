70612585316d9815ad9d19f8ce5bd4615a0fe06d
John Pham <jopham@amazon.com>
2020-02-01 18:29:05 -0800

    Add game over screen and overall car health

--- diy-or-die/Assets/Scripts/Car.cs
+++ diy-or-die/Assets/Scripts/Car.cs
@@ -5,16 +5,19 @@ using UnityEngine.UI;
 
 public class Car : MonoBehaviour, IRecepticle
 {
+    public Slider CarHealthSlider;
     public Slider TractionSlider;
     public Slider VisibilitySlider;
     public Slider TemperatureSlider;
 
+    public float CarHealth { get; set; }
     public float Traction { get; set; }
     public float Visibility { get; set; }
     public float Temperature { get; set; }
 
     private void Start()
     {
+        CarHealth = 10;
         Traction = 10;
         Visibility = 10;
         Temperature = 10;
@@ -22,13 +25,22 @@ public class Car : MonoBehaviour, IRecepticle
 
     private void Update()
     {
-        Traction -= Time.deltaTime;
-        Visibility -= Time.deltaTime;
-        Temperature -= Time.deltaTime;
+        CarHealth = ModifyHealth(CarHealth, CarHealthSlider, -Time.deltaTime * 10);
+        Traction = ModifyHealth(Traction, TractionSlider, -Time.deltaTime);
+        Visibility = ModifyHealth(Visibility, VisibilitySlider, -Time.deltaTime);
+        Temperature = ModifyHealth(Temperature, TemperatureSlider, -Time.deltaTime);
+    }
 
-        TractionSlider.value = Traction / 10;
-        VisibilitySlider.value = Visibility / 10;
-        TemperatureSlider.value = Temperature / 10;
+    // Changes the slider by the specified amount
+    // Returns the resulting value to set to the input health variable
+    // Ran into an issue modifying the input health by reference.
+    // Fix is adding return value and setting the variable with that value.
+    private float ModifyHealth(float currentHealth, Slider slider, float amount)
+    {
+        currentHealth += amount;
+        slider.value = currentHealth / 10;
+
+        return currentHealth;
     }
 
     public void ReceiveItem(Droppable item)
---
+++ diy-or-die/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+
+    public Car car;
+    public GameObject GameOverMenu;
+    public GameObject BackgroundDim;
+
+    void Update()
+    {
+        if (car.CarHealth <= 0)
+        {
+            GameOverMenu.SetActive(true);
+            BackgroundDim.SetActive(true);
+        }
+    }
+}
