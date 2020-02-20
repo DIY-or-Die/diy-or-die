f2e8e2f72f6403b391a7eed48b7fba67d109676b
Fabian <fabrofabian@gmail.com>
2020-02-20 13:38:32 -0800

    Title music added

---
+++ diy-or-die/Assets/Scripts/TitleMusic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class TitleMusic : MonoBehaviour
+{
+    EventInstance titleMusic;
+
+    EventDescription titleDesc;
+    PARAMETER_DESCRIPTION titleTrigger;
+    PARAMETER_ID titleID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleMusic = RuntimeManager.CreateInstance("event:/Music/Title");
+        titleDesc = RuntimeManager.GetEventDescription("event:/Music/Title");
+
+        titleDesc.getParameterDescriptionByName("Fade", out titleTrigger);
+        titleID = titleTrigger.id;
+
+        titleMusic.setParameterByID(titleID, 1.00f);
+
+        titleMusic.start();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log("Fade out music");
+        titleMusic.setParameterByID(titleID, 0.00f);
+    }
+}
