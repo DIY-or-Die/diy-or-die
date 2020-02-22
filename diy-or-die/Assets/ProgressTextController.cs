b7223ac2fd80fe02d0b433e3e236e034d5ad5c0f
jpham8 <newbhope8@gmail.com>
2020-02-22 10:37:04 -0800

    Add rudimentary progress text

---
+++ diy-or-die/Assets/ProgressTextController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressTextController : MonoBehaviour {
+
+    public GameManager gameManager;
+    private Text progressText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        progressText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        double milesLeft = System.Math.Floor((double) gameManager.WinTimer - gameManager.WinTimeElapsed);
+        progressText.text = "Miles To Safe House: " + milesLeft;
+    }
+}
